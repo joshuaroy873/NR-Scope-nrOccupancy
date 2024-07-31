69e92de33b141d76fe018cf7009374bbd5c1623c
Xuyang Cao <xyc@princeton.edu>
2024-07-31 13:28:06 -0400

    debug

--- nrscope/src/libs/radio_nr.cc
+++ nrscope/src/libs/radio_nr.cc
@@ -352,7 +352,7 @@ int Radio::RadioInitandStart(){
       }
 
       std::cout << "buffer rx data: " << std::endl;
-      for (int i = 0; i < rf_buffer.get_nof_samples(); i++) {
+      for (uint32_t i = 0; i < rf_buffer.get_nof_samples(); i++) {
         std::cout << rf_buffer.get(0)[i] << "; " << std::endl;
       }
       std::cout << std::endl;

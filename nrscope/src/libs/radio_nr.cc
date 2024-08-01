d4b9cac6f8117e3428f48662b4c49697244398e6
Xuyang Cao <xyc@princeton.edu>
2024-08-01 13:46:01 -0400

    fix bug

--- nrscope/src/libs/radio_nr.cc
+++ nrscope/src/libs/radio_nr.cc
@@ -328,7 +328,7 @@ int Radio::RadioInitandStart(){
   srsran_assert(ss.valid(), "Invalid synchronization raster");
 
   // initialize resampling tool
-  float r = (float)23040000/(float)33330000;       // resampling rate (output/input)
+  float r = (float)23040000/(float)33333333;       // resampling rate (output/input)
   std::cout << "[xuyang debug] r (resampling rate): " << r << std::endl;
   float As=60.0f;         // resampling filter stop-band attenuation [dB]
   msresamp_crcf q = msresamp_crcf_create(r,As);

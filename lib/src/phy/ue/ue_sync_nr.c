7ccad7011eb22a24a2944fa2c7a67d9631c0c4ac
Xuyang Cao <xyc@princeton.edu>
2024-08-08 22:25:19 -0400

    profile

--- lib/src/phy/ue/ue_sync_nr.c
+++ lib/src/phy/ue/ue_sync_nr.c
@@ -393,18 +393,18 @@ int srsran_ue_sync_nr_zerocopy_twinrx(srsran_ue_sync_nr_t* q, cf_t** buffer, srs
     return SRSRAN_ERROR;
   }
 
+  // resample 
+  u_int32_t actual_sf_sz = 0;
+  msresamp_crcf_execute(rk->resampler, buffer[0], PRE_RESAMPLING_SF_SZ, rk->temp_y, &actual_sf_sz);
+  printf("[xuyang debug] nice actual_sf_sz %u\n", actual_sf_sz);
+  srsran_vec_cf_copy(buffer[0], rk->temp_y, actual_sf_sz);
+
   // Run FSM
   switch (q->state) {
     case SRSRAN_UE_SYNC_NR_STATE_IDLE:
       // Do nothing
       break;
     case SRSRAN_UE_SYNC_NR_STATE_FIND:
-      // resample 
-      u_int32_t actual_sf_sz = 0;
-      msresamp_crcf_execute(rk->resampler, buffer[0], PRE_RESAMPLING_SF_SZ, rk->temp_y, &actual_sf_sz);
-      printf("[xuyang debug] nice actual_sf_sz %u\n", actual_sf_sz);
-      srsran_vec_cf_copy(buffer[0], rk->temp_y, actual_sf_sz);  
-
       if (ue_sync_nr_run_find(q, buffer[0]) < SRSRAN_SUCCESS) {
         ERROR("Error running find");
         return SRSRAN_ERROR;

769c9173bf14ffe44ce836df2ec142bf56b18bf5
Xuyang Cao <xyc@princeton.edu>
2024-09-04 19:28:51 -0400

    debug

--- nrscope/src/libs/radio_nr.cc
+++ nrscope/src/libs/radio_nr.cc
@@ -564,6 +564,9 @@ int Radio::FetchAndResample(){
     }
     // If in sync, update slot index. The synced data is stored in rf_buffer_t.to_cf_t()[0]
     if (outcome.in_sync){
+      if (in_sync == false) {
+        printf("in_sync change to true\n");
+      }
       in_sync = true;
       // std::cout << "System frame idx: " << outcome.sfn << std::endl;
       // std::cout << "Subframe idx: " << outcome.sf_idx << std::endl;
